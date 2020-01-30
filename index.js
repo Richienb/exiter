d569ac2bf31dc92208ab9917030308e9528fba71
Richie Bendall <richiebendall@gmail.com>
2020-01-30 18:49:30 +1300

    feat: Update code
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 "use strict"
 
-module.exports = (input, { postfix = "rainbows" } = {}) => {
-	if (typeof input !== "string") throw new TypeError(`Expected a string, got ${typeof input}`)
+const execa = require("execa")
+const platform = process.platform
 
-	return `${input} & ${postfix}`
+function getShutdownParams() {
+	if (platform === "win32" || platform === "cygwin") return ["/s", "/t", "00"]
+	else if (platform === "linux") return ["now"]
+	else if (platform === "darwin") return ["-h", "now"]
+	else if (platform === "sunos") return ["-y", "-i5", "-g0"]
+	else if (platform === "aix") return ["-Fh", "now"]
+	else if (platform === "openbsd" || platform === "freebsd" || platform === "netbsd") return ["-p", "now"]
+	else throw new Error("Operating system not supported!")
+}
+
+// function getRestartParams() {
+// 	if (platform === "win32" || platform === "cygwin") return ["/r", "/t", "00"]
+// 	else if (platform === "linux") return ["-r", "now"]
+// 	else if (platform === "darwin") return ["-r", "now"]
+// 	else if (platform === "sunos") return ["-y", "-i5", "-g0"]
+// 	else if (platform === "aix") return ["-Fh", "now"]
+// 	else if (platform === "openbsd" || platform === "freebsd" || platform === "netbsd") return ["-p", "now"]
+// 	else throw new Error("Operating system not supported!")
+// }
+
+module.exports = async () => {
+	await execa("shutdown", getShutdownParams())
+}
+
+module.exports.sync = () => {
+	execa.sync("shutdown", getShutdownParams())
 }
